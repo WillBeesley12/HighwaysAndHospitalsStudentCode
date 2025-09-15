ff82372f1b9a4427566d2dc54c978834215471f0
williambeesley <wpbeesley@gmail.com>
2025-09-15 16:21:38 -0700

    v2 - implemented Union-Find, but it doesn't work right now.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -14,57 +14,46 @@ import java.util.Set;
  */
 
 public class HighwaysAndHospitals {
-
-    /**
-     * TODO: Complete this function, cost(), to return the minimum cost to provide
-     *  hospital access for all citizens in Menlo County.
-     */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
         // When the hospital cost is less than or equal than the highway cost, we can build a hospital in every city instead.
         if (hospitalCost <= highwayCost) {
             return (long) n * hospitalCost;
         }
-        Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < cities.length; i++) {
-            int first = cities[i][0];
-            int second = cities[i][1];
-            graph.putIfAbsent(first, new ArrayList<>());
-            graph.putIfAbsent(second, new ArrayList<>());
-            graph.get(first).add(second);
-            graph.get(second).add(first);
+        // Initialize our map; for now, make every value -1 so that the "blank" nodes will have value -1 at the end.
+        int[] map = new int[n];
+        for (int i = 0; i < n; i++) {
+            map[i] = -1;
         }
-        Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
-        long highways = 0;
-        long hospitals = 0;
-        while (visited.size() < n) {
-            int random_city = 0;
-            for (int i = 1; i < n + 1; i++) {
-                if (!visited.contains(i)) {
-                    random_city = i;
-                    break;
-                }
+        // Now, iterate through each edge. Here, we will use the union find algorithm to calculate the number of disconnected subgraphs.
+        int length = cities.length;
+        for (int i = 0; i < length; i++) {
+            // I am using these while loops to make sure we find the root node of both A and B.
+            int root_A = cities[i][0];
+            while (root_A != -1) {
+                root_A = map[root_A - 1];
             }
-            visited.add(random_city);
-            queue.offer(random_city);
-            hospitals++;
-            while (!queue.isEmpty()) {
-                int queueSize = queue.size();
-                for (int j = 0; j < queueSize; j++) {
-                    int current = queue.poll();
-                    if (graph.containsKey(current)) {
-                        for (int neighbor : graph.get(current)) {
-                            if (!visited.contains(neighbor)) {
-                                highways++;
-                                visited.add(neighbor);
-                                queue.offer(neighbor);
-                            }
-                        }
-                    }
-                }
+            int root_B = cities[i][1];
+            while (root_B != -1) {
+                root_B = map[root_B - 1];
+            }
+            // Here, I am comparing th root nodes. If they are not the same, I will point the second root to the first to connect them into the same component.
+            if (root_A != root_B) {
+                map[root_B - 1] = root_A;
             }
         }
-        long answer = highways * highwayCost + hospitals * hospitalCost;
+        // Now all I have to do is count the number of components there are by going through my map and looking for "-1"s.
+        long comps = 0;
+        for (int i = 0; i < n; i++) {
+            if (map[i] == -1) {
+                comps++;
+            }
+        }
+        System.out.println(comps);
+        System.out.println(map);
+        // To compute the answer:
+        // The cost of hospitals is just the number of components times the hospital cost, as the optimal solution will have exactly one hospital in every disconnected subgraph.
+        // The cost of highways is the sum of all the "V - 1"s for each disconnected component. However, we know the sum of all Vs adds to N, so all we need to do is subtract 1 from N for each disconnected subgraph, then multiply by the cost of one highway.
+        long answer = (comps * hospitalCost) + (highwayCost * (n - comps));
         return answer;
     }
     public static void main(String[] args) {
